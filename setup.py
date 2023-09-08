ff02a6ec68f6132aa96f6beb1abff95f133f0348
Ravindra Shinde <neelravi@gmail.com>
2023-09-08 11:27:10 +0200

    version number updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='qcinfo',
-    version='0.1.0',
+    version='0.1.1',
     description='A Python package for getting insights from quantum chemical calculation data.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
